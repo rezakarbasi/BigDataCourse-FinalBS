bf40d18cee588ac33f756df515c4bb5527ecf7a8
RezaKarbasi <a.rz.karbasi@gmail.com>
2020-08-09 18:54:09 +0430

    rabbitMQ connector created .

--- ConnectRedis/connector.py
+++ ConnectRedis/connector.py
@@ -1,16 +1,27 @@
 from kafka import KafkaConsumer
 import time
+import json
+from celery import Celery
 
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                            auto_offset_reset='earliest',
-                            consumer_timeout_ms=1000)
-consumer.subscribe(['sample'])
+app = Celery('connector', broker='amqp://localhost')
 
-# while not self.stop_event.is_set():
+@app.task
+def consumer(topic='sample',port=9092):
+    consumer = KafkaConsumer(bootstrap_servers='localhost:%d' %(port),
+                                auto_offset_reset='earliest',
+                                consumer_timeout_ms=1000)
+    consumer.subscribe([topic])
 
-while True:
-    time.sleep(2)
+    # while True:
+    # time.sleep(2)
+    out = []
+    j=0
     for message in consumer:
+        j+=1
         data = json.loads(list(message)[6])
-    
-consumer.close()
+        out.append(data)
+        print(data)
+    print('-------------------------------- tedad --------------------')
+    print(j)
+    consumer.close()
+    return out
