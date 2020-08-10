d34bb31052a63e412eb4c5d30641b82b345a695d
Peyman Baghershahi <peyman.bi101@gmail.com>
2020-08-10 14:02:51 +0430

    Part4(redis) added by Peyman Bagherhshahi

--- part2_elasticsearch/elasticCommands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python preprocess.py --stopwords_file persian_stopwords.txt --setting_file setting.json
-curl -XPOST localhost:9200/messages/_close
-curl -s -H "Content-Type: application/x-ndjson" -XPUT localhost:9200/messages/_settings --data-binary @setting.json
-curl -XPOST localhost:9200/messages/_open
